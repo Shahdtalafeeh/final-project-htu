078e1e97db22d8a149f259369284a9d1ef372381
shahd_altalafeeh <shahdmoh16@gmail.com>
2022-12-04 22:13:31 +0200

    first

---
+++ src/app/core/components/layouts/side-nav-bar/side-nav-bar.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav-bar',
+  templateUrl: './side-nav-bar.component.html',
+  styleUrls: ['./side-nav-bar.component.css']
+})
+export class SideNavBarComponent implements OnInit {
+
+
+
+
+
+  constructor() {
+
+
+  }
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+
+}
